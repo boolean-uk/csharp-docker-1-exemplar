aec34f9710d2257c4e6e4fc5d83dff6d7cf5c059
uerbzr <nigel.sibbert@gmail.com>
2023-11-15 16:27:09 +0000

    updated

---
+++ README.md
@@ -0,0 +1,22 @@
+# C# Docker Exercise 1
+
+## Learning Objectives  
+
+- Deploy an application to a Docker Container  
+
+## Instructions  
+
+1. Fork this repository  
+2. Clone the fork to your machine  
+3. Open in Visual Studio  
+
+## Core 
+
+-Add the exercise.console application to a docker container
+
+## Extension
+
+-Create a Postgres container locally and update the ProductInventory methods to Create/Read/Delete to the new data store.
+
+
+
---
+++ exercise.console/Program.cs
@@ -0,0 +1,60 @@
+﻿using exercise.console.Data;
+
+bool running = true;
+
+ProductInventory.Initialize();
+
+while (running)
+{
+    
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("***************************");
+    Console.WriteLine("Welcome to the Boolean Shop");
+    Console.WriteLine("1 List Products");
+    Console.WriteLine("2 Add product");
+    Console.WriteLine("3 Delete Product");
+    Console.WriteLine("q to Quit");
+    Console.WriteLine("***************************");
+    Console.WriteLine("");
+    Console.WriteLine("");
+
+
+    string selection = Console.ReadLine();
+
+    
+    switch (selection)
+    {
+        case "1":
+            Console.Clear();
+            Console.WriteLine("Boolean Stock List");
+            Console.WriteLine("");
+            ProductInventory.Read();
+            break;
+        case "2":
+            Console.Clear();
+            Console.WriteLine("Enter Product name to add");
+            string item = Console.ReadLine();
+            ProductInventory.Create(item);
+            Console.WriteLine("Added..");
+            Thread.Sleep(1200);
+            Console.Clear();
+            break;
+        case "3":
+            Console.Clear();
+            Console.WriteLine("Enter Product Id to delete");
+            ProductInventory.Read();
+            string id = Console.ReadLine();
+            ProductInventory.Delete(int.Parse(id));
+            Console.WriteLine("Deleted..");
+            Thread.Sleep(1200);
+            Console.Clear();
+            break;
+        case "q":
+        case "Q":
+            return;
+        default:
+            break;
+            
+    }
+}
